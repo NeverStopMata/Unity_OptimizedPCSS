92a2aa19ff9459651f707e3a44697b5b37bf45fc
NeverStopMata <matanan@outlook.com>
2019-01-01 23:27:08 +0800

    :rocket: Add the basement of the SRP

---
+++ Assets/Scripts/MataRenderPipeline.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering;
+
+
+
+public class MataRenderPipeline : RenderPipeline
+{
+    private readonly MataRenderPipelineAsset pipelineAsset;
+
+    public MataRenderPipeline(MataRenderPipelineAsset asset)
+    {
+        pipelineAsset = asset;
+    }
+    // Start is called before the first frame update
+    public override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        base.Render(context, cameras);
+        var clearCmd = new CommandBuffer { name = "Clear(SRP-CommandBuffer)" };
+        clearCmd.ClearRenderTarget(true, true, pipelineAsset.clear);
+        context.ExecuteCommandBuffer(clearCmd);
+        clearCmd.Release();//dispose
+        context.Submit();
+    }
+}
---
+++ Assets/Scripts/MataRenderPipelineAsset.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEditor;
+
+[ExecuteInEditMode]
+[CreateAssetMenu(fileName = "MataRenderPipelineAsset.asset", menuName = "SRPAssets/MataRenderPipeline", order = 1)]
+public class MataRenderPipelineAsset : RenderPipelineAsset
+{
+    public Color clear = Color.green;
+
+#if UNITY_EDITOR
+    static void CreateSRP()
+    {
+        var instance = ScriptableObject.CreateInstance<MataRenderPipelineAsset>();
+        if (!AssetDatabase.IsValidFolder("Assets/SRPAssets"))
+            AssetDatabase.CreateFolder("Assets", "SRPAssets");
+        AssetDatabase.CreateAsset(instance, "Assets/SRPAssets/MataRenderPipelineAsset.asset");
+    }
+#endif
+
+    protected override IRenderPipeline InternalCreatePipeline()
+    {
+        return new MataRenderPipeline(this);
+    }
+}
